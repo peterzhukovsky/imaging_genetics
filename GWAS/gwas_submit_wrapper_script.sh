59c7f84b944f32d937d6995302a7ebbe977596ae
Peter Zhukovsky <38474927+peterzhukovsky@users.noreply.github.com>
2023-05-01 15:12:41 -0400

    Update gwas_submit_wrapper_script.sh

--- GWAS/gwas_submit_wrapper_script.sh
+++ GWAS/gwas_submit_wrapper_script.sh
@@ -11,3 +11,19 @@ done
 for i in `seq 2 34`; do 
 sbatch --mem=80G -n 1 -c 13 $SCRATCH/biobank/genetics/EURnonWB/WB/sbatch_gwas${i}.sh
 done
+
+
+### METAL section
+
+for i in `seq 1 34`; do 
+cp $SCRATCH/biobank/genetics/EURnonWB/METAL/metal_script.txt $SCRATCH/biobank/genetics/EURnonWB/METAL/metal_script${i}.txt
+sed -i -e "s/X_roi_X/${i}/g" $SCRATCH/biobank/genetics/EURnonWB/METAL/metal_script${i}.txt
+done
+
+for i in `seq 1 34`; do 
+metal $SCRATCH/biobank/genetics/EURnonWB/METAL/metal_script${i}.txt
+done
+
+for i in `seq 1 34`; do 
+gzip METAANALYSIS_CT${i}_WB_EUR_1.txt
+done
