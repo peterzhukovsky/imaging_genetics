1627ac8f5fde8b06f2db0f7fc1fcc2b0687d7662
Peter Zhukovsky <38474927+peterzhukovsky@users.noreply.github.com>
2023-05-01 15:36:33 -0400

    Update CLSA_PRS.m

--- GxE/CLSA/CLSA_PRS.m
+++ GxE/CLSA/CLSA_PRS.m
@@ -3,56 +3,65 @@ cd D:\Canada_2020\CLSA
 
 clsa_base=readtable('D:\Canada_2020\CLSA\data\clsa_dru_bl_cog1.csv'); clsa_base=sortrows(clsa_base, 2);
 clsa_snps=readtable('D:\Canada_2020\CLSA\data\CLSA_dosage.csv');clsa_snps=sortrows(clsa_snps, 2);
-clsa_prs=readtable('D:\Canada_2020\CLSA\data\clsa_ct_prs.csv');clsa_prs(26623:end,:) = [];clsa_prs.ADM_GWAS_COM=(clsa_prs.FID);
-clsa_snps=innerjoin(clsa_snps, clsa_prs);
-clsa_snps(:,'ADM_GWAS_COM') = [];clsa_snps(:,'ADM_GWAS3') = [];
+clsa_snps(:,'ADM_GWAS3') = [];clsa_snps(30098:end,:) = [];
 clsa_base.CCC_HBP_COM(clsa_base.CCC_HBP_COM==2)=0;
 clsa_base.CCC_HEART_COM(clsa_base.CCC_HEART_COM>3)=NaN;clsa_base.CCC_HBP_COM(clsa_base.CCC_HBP_COM>3)=NaN; clsa_base.CCC_ANGI_COM(clsa_base.CCC_ANGI_COM>3)=NaN; clsa_base.CCC_AMI_COM(clsa_base.CCC_AMI_COM>3)=NaN;
 clsa_base.DEP=clsa_base.DEP_CESD10_COM>10;
 clsa_base.STP_INTFR_RATIO_COM(clsa_base.STP_INTFR_RATIO_COM>10)=NaN;
+%clsa_joint.ED_UDR11_COM(clsa_joint.ED_UDR11_COM>40)=NaN;
 
+gwas_weights=readtable('D:\Canada_2020\UK_biobank\reports\AD\genetics\reports\summary_stats_ALL_cut.xlsx');%gwas_weights=readtable('D:\Canada_2020\CLSA\data\summary_stats_for_prs.xlsx');
+gwas_alleles=readtable('D:\Canada_2020\CLSA\data\clsa_A1_REF.csv'); 
+g=readtable('D:\Canada_2020\CLSA\data\summary_stats_for_prs.xlsx');
+%gwas_weights.Zscore(strcmp(gwas_weights.A1, gwas_weights.COUNTED)
 
-clsa_joint=innerjoin(clsa_base, clsa_snps); %clsa_joint.cardio=clsa_joint.CCC_HEART==1 | clsa_joint.CCC_HBP==1 | clsa_joint.CCC_AMI==1 | clsa_joint.CCC_ANGI==1;
-clsa_joint.ED_UDR11_COM(clsa_joint.ED_UDR11_COM>40)=NaN;
+for t=[1:25,27:34]
+snps=gwas_weights.rsID(gwas_weights.Trait_1==t);empty=zeros(1,length(snps));
+clear tmp weights; for i=1:length(snps); i
+    try
+    ix=strcmp(clsa_snps.Properties.VariableNames, snps(i));
+    tmp(:,i)=clsa_snps(:,ix);
+    
+    ix=strcmp(g.MarkerName, snps(i));
+    weights(:,i)=repmat(g.Zscore_aligned_to_CLSA_A1(ix), [30097,1]);
+    
+    catch
+    tmp{:,i}=zeros(30097,1); weights(:,i)=zeros(30097,1);%empty(i)=1
+    end
+end
+prs_thick(:,t)=nansum((tmp{:,:}.*weights)')'; %prs_thick(:,t)=prs_thick(:,t)./nanmean(prs_thick(:,t) );
+prs_thick(prs_thick(:,t)==0,t)=NaN;
+PCAs=[clsa_snps.PC1,clsa_snps.PC2,clsa_snps.PC3,clsa_snps.PC4,clsa_snps.PC5,clsa_snps.PC6,clsa_snps.PC7,clsa_snps.PC8,clsa_snps.PC9,clsa_snps.PC10];
+%[b,bint,r] = regress(prs_thick(:,t),PCAs); %prs_thick(:,t)=r; %figure; scatter(r, prs_thick(:,i)); 
+end
+prs_thick(:,26)=[];
+
+clsa_joint=clsa_base; %clsa_joint.cardio=clsa_joint.CCC_HEART==1 | clsa_joint.CCC_HBP==1 | clsa_joint.CCC_AMI==1 | clsa_joint.CCC_ANGI==1;
 
 regions_short={'GlobalMeanMean thickness';'STS';'cACC';'cMFG';'cuneus';'entorhinal';'fusiform';'iparietal';'itemporal';'isthmuscingulate';'loccipital';'lorbitofrontal';'lingual';'morbitofrontal';'mtemporal';'phippocampal';'pocentral';'popercularis';'porbitalis';'ptriangularis';'pcalcarine';'pcentral';'pcingulate';'precentral';'precuneus';'rACC';'rMFG';'SupF';'SupPar';'SupT';'SupMarg';'fPole';'trTemp';'insula'};
-
-%clsa_joint.cardio=clsa_joint.CCC_HEART_COM==1 | clsa_joint.CCC_HBP_COM==1 | clsa_joint.CCC_AMI_COM==1 | clsa_joint.CCC_ANGI_COM==1;
+regions_short(26)=[];
+clsa_joint.cardio=clsa_joint.CCC_HEART_COM==1 | clsa_joint.CCC_HBP_COM==1 | clsa_joint.CCC_AMI_COM==1 | clsa_joint.CCC_ANGI_COM==1;
 
 %%
-% for i in `seq 1 34`; do head -n 26622 CT${i}.all_score > CT${i}.all_score.txt; done
-% for i in `seq 1 34`; do sed -ie 's/[ ][ ]*/ /g' CT${i}.all_score.txt ; done
 clear snp_*
-for i=1:34; i
-prs=readtable(strcat('D:\Canada_2020\CLSA\data\PRS\CT', num2str(i),'.all_score.txt'), 'Delimiter', ' ');
-prs.Properties.VariableNames={'FID', 'IID', 'Pt_5e08', 'Pt_5e07', 'Pt_5e06', 'Pt_5e05', 'Pt_0p0005', 'Pt_0p005', 'Pt_0p05', 'Pt_0p1', 'Pt_0p5', 'Pt_1', 'Missing'};
-prs.ADM_GWAS_COM=prs.FID;
-T=innerjoin(clsa_joint, prs);
-    mdl=fitlm(T, 'COG_CONSTR_EF4_COM ~ Pt_5e08 + AGE_NMBR_COM + ED_UDR11_COM + SEX_ASK_COM + PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10');
-    snp_stats_main(i,:)=mdl.Coefficients(strcmp(mdl.Coefficients.Properties.RowNames,'Pt_5e08'),:);
-    mdl=fitlm(T, 'COG_CONSTR_EF4_COM ~ Pt_5e08*CCC_HBP_COM+ AGE_NMBR_COM + ED_UDR11_COM + SEX_ASK_COM+ PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10');%+ PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10
-    snp_stats_int(i,:)=mdl.Coefficients(strcmp(mdl.Coefficients.Properties.RowNames,'CCC_HBP_COM:Pt_5e08'),:);
-    mdl=fitlm(T, 'COG_CONSTR_EF4_COM ~ Pt_5e08*DEP+ AGE_NMBR_COM + ED_UDR11_COM + SEX_ASK_COM+ PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10');%+ PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10
-    snp_stats_int_DEP(i,:)=mdl.Coefficients(strcmp(mdl.Coefficients.Properties.RowNames,'DEP_1:Pt_5e08'),:);
-    
-    mdl=fitlm(T, 'COG_CONSTR_EF4_COM ~ Pt_5e05 + AGE_NMBR_COM + ED_UDR11_COM + SEX_ASK_COM + PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10');
-    snp_stats_main5e05(i,:)=mdl.Coefficients(strcmp(mdl.Coefficients.Properties.RowNames,'Pt_5e05'),:);
-    mdl=fitlm(T, 'COG_CONSTR_EF4_COM ~ Pt_0p005 + AGE_NMBR_COM + ED_UDR11_COM + SEX_ASK_COM + PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10');
-    snp_stats_main0p005(i,:)=mdl.Coefficients(strcmp(mdl.Coefficients.Properties.RowNames,'Pt_0p005'),:);
-    mdl=fitlm(T, 'COG_CONSTR_EF4_COM ~ Pt_5e07 + AGE_NMBR_COM + ED_UDR11_COM + SEX_ASK_COM + PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10');
-    snp_stats_main5e07(i,:)=mdl.Coefficients(strcmp(mdl.Coefficients.Properties.RowNames,'Pt_5e07'),:);
-    mdl=fitlm(T, 'COG_CONSTR_EF4_COM ~ Pt_5e06 + AGE_NMBR_COM + ED_UDR11_COM + SEX_ASK_COM + PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10');
-    snp_stats_main5e06(i,:)=mdl.Coefficients(strcmp(mdl.Coefficients.Properties.RowNames,'Pt_5e06'),:);
-    
+for i=1:33; i
+    T=table(clsa_joint.CCC_HBP_COM, clsa_joint.DEP, clsa_joint.entity_id,clsa_joint.COG_CONSTR_EF4_COM,clsa_joint.AGE_NMBR_COM,clsa_joint.ED_UDR11_COM,clsa_joint.SEX_ASK_COM, clsa_snps.PC1,clsa_snps.PC2,clsa_snps.PC3,clsa_snps.PC4,clsa_snps.PC5,clsa_snps.PC6,clsa_snps.PC7,clsa_snps.PC8,clsa_snps.PC9,clsa_snps.PC10, (prs_thick(:,i)),...
+        'VariableNames', {'hypert', 'DEP', 'entity_id_clsa_long','COG_REYII_SCORE','AGE_BL','ED_UDR11','SEX_ASK','PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10','SNP'});
+    mdl=fitlm(T, 'COG_REYII_SCORE ~ SNP+ AGE_BL + ED_UDR11 + SEX_ASK + PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10');
+    snp_stats_main(i,:)=mdl.Coefficients(strcmp(mdl.Coefficients.Properties.RowNames,'SNP'),:);
+    mdl=fitlm(T, 'COG_REYII_SCORE ~ SNP*hypert+ AGE_BL + ED_UDR11 + SEX_ASK+ PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10');%+ PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10
+    snp_stats_int(i,:)=mdl.Coefficients(strcmp(mdl.Coefficients.Properties.RowNames,'hypert:SNP'),:);
+    mdl=fitlm(T, 'COG_REYII_SCORE ~ SNP*DEP+ AGE_BL + ED_UDR11 + SEX_ASK  + PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9+PC10');
+    snp_stats_int_DEP(i,:)=mdl.Coefficients(strcmp(mdl.Coefficients.Properties.RowNames,'DEP_1:SNP'),:);
 end
 
 regions_short(mafdr(snp_stats_main.pValue, 'BHFDR', true)<0.1)
-regions_short(mafdr(snp_stats_int.pValue, 'BHFDR', true)<0.1)
-regions_short(mafdr(snp_stats_int_DEP.pValue, 'BHFDR', true)<0.1)
+regions_short(mafdr(snp_stats_int.pValue, 'BHFDR', false)<0.1)
+regions_short(mafdr(snp_stats_int_DEP.pValue)<0.1)
 
 snp_stats_main(1,:)=[];snp_stats_int(1,:)=[];snp_stats_int_DEP(1,:)=[];
-snp_stats_main.region=regions_short; snp_stats_main.pfdr=mafdr(snp_stats_main.pValue, 'BHFDR', true);
-snp_stats_int.pfdr=mafdr(snp_stats_int.pValue,'BHFDR', false);
+snp_stats_main.pfdr=mafdr(snp_stats_main.pValue, 'BHFDR', true);
+snp_stats_int.pfdr=mafdr(snp_stats_int.pValue,'BHFDR', true);
 snp_stats_int_DEP.pfdr=mafdr(snp_stats_int_DEP.pValue, 'BHFDR', true);
 
 i=3 %19 %20
